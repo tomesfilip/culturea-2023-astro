50dc2961ea166004c7b2e6a1e99ea054a4c47bda
Filip Tomeš <tomes@cngroup.dk>
2023-01-19 21:11:53 +0200

    using client side fetching as the server side has problems

--- src/components/program/InstagramFeed.tsx
+++ src/components/program/InstagramFeed.tsx
@@ -1,24 +1,16 @@
-const res = await fetch(
-  `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=
-  }`,
-  { cache: 'no-store' }
-);
-const { data } = await res.json();
-
-const instagramPhotoFeed = data.filter(
-  (item: any) =>
-    item.media_type === 'CAROUSEL_ALBUM' || item.media_type === 'IMAGE'
-);
+import useFetchRecipeList from '../../hooks/useFetchIgFeed';
 
 const InstagramFeed = () => {
+  const { data: igPhotoFeed, isLoading, error } = useFetchRecipeList();
+
   return (
     <>
       <h4 className="my-6 text-lg text-center uppercase">
         Pro nejnovější informace nás sledujte na instagramu
       </h4>
       <div className="grid justify-center grid-cols-1 gap-4 md:grid-cols-3">
-        {instagramPhotoFeed &&
-          instagramPhotoFeed.slice(0, 3)?.map((feedItem: any) => (
+        {igPhotoFeed &&
+          igPhotoFeed.slice(0, 3)?.map((feedItem: any) => (
             <a
               key={feedItem.id}
               href={feedItem.permalink}
