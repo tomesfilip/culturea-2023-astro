d454c8544447da5968ff0e0d944522159267a964
Filip Tomeš <fitome@ttu.ee>
2023-02-15 16:54:38 +0200

    scrollable blog content

--- src/components/blogs/BlogDetailModal.tsx
+++ src/components/blogs/BlogDetailModal.tsx
@@ -18,20 +18,19 @@ const BlogDetailModal = ({ blog, closeModalOnClick }: Props) => {
     <ModalOverlay>
       <div className="flex flex-col items-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20 bg-white rounded-lg p-4 md:p-8 w-11/12 md:w-4/5 lg:w-3/5 h-[90vh] md:h-[85vh] lg:h-[80vh]">
         <ModalHeader closeModal={closeModalOnClick} />
-        <h3 className="w-full text-center lg:text-left">{blog.title}</h3>
-        <img
-          className="mt-2 mb-3 max-w-[300px] max-h-[300px] rounded-lg object-cover"
-          src={blog.bannerImage}
-          alt={blog.title}
-          width={300}
-          height={300}
-        />
-        <div className="blog-body-text overflow-y-scroll max-h-[600px]">
-          {parse(blog.body)}
+        <div className="blog-content overflow-y-scroll mt-2">
+          <h3 className="w-full text-center lg:text-left">{blog.title}</h3>
+          <img
+            className="mt-2 mb-3 max-w-[300px] max-h-[300px] rounded-lg object-cover"
+            src={blog.bannerImage}
+            alt={blog.title}
+            width={300}
+            height={300}
+          />
+          <div className="blog-body-text">{parse(blog.body)}</div>
         </div>
         {loggedUser && <EditDeleteActions blogId={blog.id} />}
       </div>
-      )
     </ModalOverlay>
   );
 };
