6e240a08c7236032c61a48caf918ffd0ac8fd331
Filip Tomeš <fitome@ttu.ee>
2023-02-15 16:42:26 +0200

    detail modal styling updates

--- src/components/blogs/BlogDetailModal.tsx
+++ src/components/blogs/BlogDetailModal.tsx
@@ -16,7 +16,7 @@ const BlogDetailModal = ({ blog, closeModalOnClick }: Props) => {
 
   return (
     <ModalOverlay>
-      <div className="flex flex-col items-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20 bg-white rounded-lg p-4 md:p-8 w-11/12 md:w-4/5 lg:w-3/5 h-[75vh]">
+      <div className="flex flex-col items-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20 bg-white rounded-lg p-4 md:p-8 w-11/12 md:w-4/5 lg:w-3/5 h-[90vh] md:h-[85vh] lg:h-[80vh]">
         <ModalHeader closeModal={closeModalOnClick} />
         <h3 className="w-full text-center lg:text-left">{blog.title}</h3>
         <img
